8266af77ec45e6d8fd7482099cfec2d7251bb450
IamTheRafsane <sending.to.evan@gmail.com>
2023-10-14 11:33:18 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace program
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Total how many numbers to be calculated?: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int res = a;
+
+            for (int x = 0; x < num - 1; x++)
+            {
+                string symbol = Console.ReadLine();
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                if (symbol.Equals("+"))
+                {
+                    res = res + b;
+                }
+                if (symbol.Equals("-"))
+                {
+                    res = res - b;
+                }
+                if (symbol.Equals("*"))
+                {
+                    res = res * b;
+                }
+                if (symbol.Equals("/"))
+                {
+                    if (b == 0)
+                    {
+                        Console.WriteLine("Division by zero is not allowed.");
+                        break;
+                    }
+                    res = res / b;
+
+                }
+            }
+
+            Console.WriteLine("Result: " + res);
+
+
+
+
+
+        }
+    }
+}
